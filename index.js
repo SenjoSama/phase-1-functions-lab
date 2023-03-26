f8ca9761946dc6f031a91667ab6ed11ee9a81668
Trenton Jones <mr.jones1213@yahoo.com>
2023-03-26 15:01:16 -0400

    sub for lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const hq = 42;
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - hq);
+}
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock = 264;
+    let distance
+    if (start >= destination){
+        distance = (start - destination) * feetPerBlock;
+    } else {
+        distance = (destination - start) * feetPerBlock;
+    }
+    return distance;
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    const freeRide = 400;
+    let price;
+    if (distance <= freeRide){
+        price = 0;
+    } else if (distance > 400 && distance < 2000){
+        price = (distance - freeRide) * 0.02;
+    } else if (distance > 2000 && distance < 2500){
+        price = 25;
+    } else 
+        price = "cannot travel that far"
+    
+    return price;
+}
